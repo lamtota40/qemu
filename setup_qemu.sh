11d2e7a594c188d17b952fe559c6eb06d8b64dbf
lamtota40 <lamtota40@gmail.com>
2025-05-07 19:52:14 +0700

    Update setup_qemu.sh

--- setup_qemu.sh
+++ setup_qemu.sh
@@ -1,29 +1,65 @@
 #!/bin/bash
 
-# Path untuk file ISO Lubuntu dan image disk
+# Konfigurasi
 LUBUNTU_ISO_URL="https://cdimage.ubuntu.com/lubuntu/releases/18.04/release/lubuntu-18.04-alternate-amd64.iso"
 LUBUNTU_ISO="/tmp/lubuntu-18.04-alternate-amd64.iso"
 LUBUNTU_IMG="/root/lubuntu.img"
+DISK_SIZE=19G
+VNC_PASSWORD="pas123"
+MONITOR_SOCKET="/tmp/qemu-monitor.sock"
 
-# Ukuran disk image dalam GB
-DISK_SIZE=19
+# Install QEMU dan dependensi
+echo "[1/6] Memasang QEMU dan dependensi..."
+apt update
+apt install -y qemu qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils virt-manager wget x11vnc socat
 
-# Install QEMU dan dependensi yang diperlukan
-echo "Memasang QEMU dan dependensi..."
-sudo apt update
-sudo apt install -y qemu qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils virt-manager wget x11vnc
+# Download ISO jika belum ada
+echo "[2/6] Mengecek ISO Lubuntu..."
+if [ ! -f "$LUBUNTU_ISO" ]; then
+  echo "Mengunduh ISO Lubuntu..."
+  wget -O "$LUBUNTU_ISO" "$LUBUNTU_ISO_URL"
+else
+  echo "ISO sudah tersedia di $LUBUNTU_ISO"
+fi
 
-# Mengunduh ISO Lubuntu
-echo "Mengunduh ISO Lubuntu 18.04..."
-wget -O "$LUBUNTU_ISO" "$LUBUNTU_ISO_URL"
+# Buat image jika belum ada
+echo "[3/6] Mengecek image disk..."
+if [ ! -f "$LUBUNTU_IMG" ]; then
+  echo "Membuat disk image $DISK_SIZE..."
+  qemu-img create -f raw "$LUBUNTU_IMG" $DISK_SIZE
+else
+  echo "Image sudah ada di $LUBUNTU_IMG"
+fi
 
-# Membuat image disk untuk Lubuntu
-echo "Membuat image disk untuk Lubuntu dengan ukuran $DISK_SIZE GB..."
-qemu-img create -f raw "$LUBUNTU_IMG" "${DISK_SIZE}G"
-
+# Simpan password VNC
+echo "[4/6] Menyimpan password VNC..."
 mkdir -p /root/.vnc
-x11vnc -storepasswd $VNC_PASSWORD /root/.vnc/passwd
+x11vnc -storepasswd "$VNC_PASSWORD" /root/.vnc/passwd
 
+# Jalankan QEMU di background
+echo "[5/6] Menjalankan QEMU dengan VNC dan monitor socket..."
+qemu-system-x86_64 \
+  -m 1024 \
+  -smp 1 \
+  -cpu host \
+  -enable-kvm \
+  -hda "$LUBUNTU_IMG" \
+  -cdrom "$LUBUNTU_ISO" \
+  -boot d \
+  -vnc :1,password \
+  -k en-us \
+  -net nic \
+  -net user \
+  -monitor unix:$MONITOR_SOCKET,server,nowait &
 
+sleep 5
 
+# Set password VNC melalui monitor QEMU
+echo "[6/6] Mengatur password VNC via monitor socket..."
+{
+  echo "change vnc password"
+  echo "$VNC_PASSWORD"
+} | socat - UNIX-CONNECT:$MONITOR_SOCKET
 
+echo "Selesai. QEMU berjalan dengan VNC di localhost:5901"
+echo "Gunakan VNC Viewer dan login dengan password: $VNC_PASSWORD"
