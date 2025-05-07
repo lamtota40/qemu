f5d5f36b5bdd28409e6ec12d693e8410dc2cbad8
lamtota40 <lamtota40@gmail.com>
2025-05-08 05:27:54 +0700

    Update afterfinishinstall.sh

--- afterfinishinstall.sh
+++ afterfinishinstall.sh
@@ -2,23 +2,7 @@ pkill qemu-system-x86_64
 ps aux | grep qemu
 kill -9 7195
 
-qemu-system-x86_64 \
-  -m 1024 \
-  -smp 1 \
-  -cpu host \
-  -enable-kvm \
-  -hda /root/lubuntu.img \
-  -boot c \
-  -vnc :1,password \
-  -k en-us \
-  -net nic \
-  -net user \
-  -monitor unix:/tmp/qemu-monitor.sock,server,nowait &
-
-{
-  echo "change vnc password"
-  echo "pas123"
-} | socat - UNIX-CONNECT:/tmp/qemu-monitor.sock
+qemu-img create -f qcow2 /root/lubuntu.qcow2 18G
 
 qemu-system-x86_64 \
   -m 1024 \
@@ -33,6 +17,14 @@ qemu-system-x86_64 \
   -device e1000,netdev=mynet \
   -monitor unix:/tmp/qemu-monitor.sock,server,nowait &
 
+  {
+  echo "change vnc password"
+  echo "pas123"
+} | socat - UNIX-CONNECT:/tmp/qemu-monitor.sock
 
-dd if=/root/lubuntu.img bs=1M | gzip > /root/lubuntu-backup.img.gz
-gzip -dc /root/lubuntu-backup.img.gz | dd of=/root/lubuntu.img bs=1M status=progress
+#kompres&backup
+qemu-img convert -c -O qcow2 /root/lubuntu.qcow2 /root/lubuntu-compress.qcow2
+
+#restore
+qemu-img convert -O raw /root/compress.qcow2 - | dd of=/dev/vda bs=4M status=progress
+sync
