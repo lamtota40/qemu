d459d359392afed5d81ef4c8677d4b7cccaa52a3
lamtota40 <lamtota40@gmail.com>
2025-05-08 07:23:35 +0700

    Update setup_qemu.sh

--- setup_qemu.sh
+++ setup_qemu.sh
@@ -3,7 +3,7 @@
 # Konfigurasi
 LUBUNTU_ISO_URL="https://cdimage.ubuntu.com/lubuntu/releases/18.04/release/lubuntu-18.04-alternate-amd64.iso"
 LUBUNTU_ISO="/tmp/lubuntu-18.04-alternate-amd64.iso"
-LUBUNTU_IMG="/root/lubuntu.img"
+LUBUNTU_IMG="/root/lubuntu.qcow2"
 DISK_SIZE=18G
 VNC_PASSWORD="pas123"
 MONITOR_SOCKET="/tmp/qemu-monitor.sock"
