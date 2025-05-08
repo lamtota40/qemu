5a42fdae1421bc456e69dfb44659856452fa6598
lamtota40 <lamtota40@gmail.com>
2025-05-08 19:04:39 +0700

    Create info.sh

---
+++ info.sh
@@ -0,0 +1,15 @@
+
+Unduh ISO Windows XP SP3 Professional 32-bit
+Nama file:en_windows_xp_professional_with_service_pack_3_x86_cd_vl_x14-73974.iso
+Ukuran: Sekitar 589 MB
+5bf476e2fc445b8d06c13b073ecf1944
+Halaman arsip: https://archive.org/details/WinXPProSP3x86
+
+Tanggal rilis: April 2005 (SP2 untuk x64)
+Build number: 5.2.3790.3959 (berbeda dari XP 32-bit)
+Nama file ISO yang umum: en_win_xp_pro_x64_with_sp2.iso
+MD5 Hash Resmi ISO XP x64 SP2
+7d9d92b96f6e4b0fbbf93d64d5105c68
+Ukuran file: Sekitar 620 MB
+Versi bahasa: English (US)
+Jenis lisensi: Retail (bukan volume license)
