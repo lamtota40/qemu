7ecc1325edd2f7addab92ce98cb87f0818e0e584
lamtota40 <lamtota40@gmail.com>
2025-05-09 02:09:26 +0700

    Update setup_xp.sh

--- setup_xp.sh
+++ setup_xp.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Konfigurasi
-WINXP_ISO_URL="https://cdimage.ubuntu.com/lubuntu/releases/18.04/release/lubuntu-18.04-alternate-amd64.iso"
-WINXP_ISO="/tmp/lubuntu-18.04-alternate-amd64.iso"
-WINXP_IMG="/root/winxp.qcow2"
-DISK_SIZE=18G
+WINXP_ISO_URL="https://archive.org/download/WinXPProSP3x86/WinXPProSP3x86.iso"
+WINXP_ISO="/tmp/windows-xp.iso"
+WINXP_IMG="/root/windows-xp.qcow2"
+DISK_SIZE=10G
 VNC_PASSWORD="pas123"
 MONITOR_SOCKET="/tmp/qemu-monitor.sock"
 
@@ -14,21 +14,21 @@ apt update
 apt install -y qemu qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils virt-manager wget x11vnc socat
 
 # Download ISO jika belum ada
-echo "[2/6] Mengecek ISO Lubuntu..."
-if [ ! -f "$LUBUNTU_ISO" ]; then
-  echo "Mengunduh ISO Lubuntu..."
-  wget -O "$LUBUNTU_ISO" "$LUBUNTU_ISO_URL"
+echo "[2/6] Mengecek ISO Windows XP..."
+if [ ! -f "$WINXP_ISO" ]; then
+  echo "Mengunduh ISO Windows XP..."
+  wget -O "$WINXP_ISO" "$WINXP_ISO_URL"
 else
-  echo "ISO sudah tersedia di $LUBUNTU_ISO"
+  echo "ISO sudah tersedia di $WINXP_ISO"
 fi
 
 # Buat image jika belum ada
 echo "[3/6] Mengecek image disk..."
-if [ ! -f "$LUBUNTU_IMG" ]; then
+if [ ! -f "$WINXP_IMG" ]; then
   echo "Membuat disk image $DISK_SIZE..."
-  qemu-img create -f qcow2 "$LUBUNTU_IMG" $DISK_SIZE
+  qemu-img create -f qcow2 "$WINXP_IMG" $DISK_SIZE
 else
-  echo "Image sudah ada di $LUBUNTU_IMG"
+  echo "Image sudah ada di $WINXP_IMG"
 fi
 
 # Simpan password VNC
@@ -39,12 +39,11 @@ x11vnc -storepasswd "$VNC_PASSWORD" /root/.vnc/passwd
 # Jalankan QEMU di background
 echo "[5/6] Menjalankan QEMU dengan VNC dan monitor socket..."
 qemu-system-x86_64 \
-  -m 1024 \
+  -m 512 \
   -smp 1 \
-  -cpu host \
-  -enable-kvm \
-  -hda "$LUBUNTU_IMG" \
-  -cdrom "$LUBUNTU_ISO" \
+  -cpu pentium \
+  -hda "$WINXP_IMG" \
+  -cdrom "$WINXP_ISO" \
   -boot d \
   -vnc :1,password \
   -k en-us \
@@ -61,5 +60,5 @@ echo "[6/6] Mengatur password VNC via monitor socket..."
   echo "$VNC_PASSWORD"
 } | socat - UNIX-CONNECT:$MONITOR_SOCKET
 
-echo "Selesai. QEMU berjalan dengan VNC di localhost:5901"
+echo "Selesai. Windows XP berjalan dengan VNC di localhost:5901"
 echo "Gunakan VNC Viewer dan login dengan password: $VNC_PASSWORD"
