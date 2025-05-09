d4c5dc5407d86789e2ebfd23374cb995cbd6b7df
lamtota40 <lamtota40@gmail.com>
2025-05-09 21:54:23 +0700

    Update setup_qemu.sh

--- setup_qemu.sh
+++ setup_qemu.sh
@@ -2,7 +2,7 @@
 
 # Konfigurasi
 LUBUNTU_ISO_URL="https://cdimage.ubuntu.com/lubuntu/releases/18.04/release/lubuntu-18.04-alternate-amd64.iso"
-LUBUNTU_ISO="/tmp/lubuntu-18.04-alternate-amd64.iso"
+LUBUNTU_ISO="/root/lubuntu-18.04-alternate-amd64.iso"
 LUBUNTU_IMG="/root/lubuntu.qcow2"
 DISK_SIZE=18.6G
 VNC_PASSWORD="pas123"
